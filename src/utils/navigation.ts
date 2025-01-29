7f6ba08d3e9a67322c2a801415fda12be69c1ce5
Nathan Casler <nathan.casler@turbaline.com>
2025-01-29 13:42:49 -0700

    Replace images

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,24 +1,15 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Services", url: "/services" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Services", url: "/services" },
-    ],
-  },
-  {
     section: "Company",
     links: [
       { name: "About us", url: "#" },
       { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
     ],
   },
 ];
